70a9c3d2277653501fdc4714e69fd298d012f80d
DESKTOP-2OMHMNV\김지현 <jkim68888@gmail.com>
2021-03-06 11:03:56 +0900

    today's best부분 구현중

--- js/main.js
+++ js/main.js
@@ -17,3 +17,30 @@ $(document).ready(function(){
         },
     });
 });
+
+
+//today_best
+$(document).ready(function(){
+    var tabbar = '.today_best .tabbar li';
+    var content01 = '.today_best .contents01';
+    var content02 = '.today_best .contents02';
+
+    $(tabbar).eq(0).find('a').click(function(e){
+        e.preventdefault();
+        
+        $(content02).stop().fadeOut('fast');
+        $(content01).stop().fadeIn('fast');
+        $(tabbar).eq(1).removeClass('active');
+        $(tabbar).eq(0).addClass('active');
+    });
+
+    $(tabbar).eq(1).find('a').click(function(e){
+        e.preventdefault();  
+        
+        $(content01).stop().fadeOut('fast');
+        $(content02).stop().fadeIn('fast');
+        $(tabbar).eq(0).removeClass('active');
+        $(tabbar).eq(1).addClass('active');
+    });
+    
+});
