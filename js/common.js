e4d095f32f591556f7aff7682edf0fd5e7f20e47
DESKTOP-2OMHMNV\김지현 <jkim68888@gmail.com>
2021-03-08 17:40:24 +0900

    모바일 패널 작업중

--- js/common.js
+++ js/common.js
@@ -26,3 +26,65 @@ $(document).ready(function(){
     });
 
 });
+
+//gnb
+$(document).ready(function(){
+    var main = 'header .main_nav';
+    var sub = 'header .sub_nav';
+    var open = 'header .panel_ico';
+    var close = 'header .close_btn';
+    
+    //pc이벤트함수
+    function pcGnb(){
+        $(main).off('click');
+        
+        $(main).hover(function(){
+            $(this).next().stop().slideDown('fast'); 
+        },function(){
+            $(this).next().stop().slideUp('fast'); 
+        });
+    }
+    
+    
+    //모바일이벤트함수
+    function mGnb(){
+        $(main).off('mouseenter');
+        $(main).off('mouseleave');
+        
+        $(open).find('a').click(function(){
+            $(this).addClass('active');
+        });
+
+        $(close).find('a').click(function(){
+            $(this).addClass('active');
+        });
+        
+        $(main).off('click');
+
+        $(main).click(function(){
+            $(sub).stop().slideUp('fast');
+            $(this).next().stop().slideDown('fast'); 
+        },function(){
+            $(this).next().stop().slideUp('fast'); 
+        });
+        
+    }
+    
+    $(window).resize(function(){
+        $(open, close).removeClass('active');
+        $(sub).slideUp(0);
+        
+        var w = window.innerWidth; 
+        
+        if(w >= 1024){ 
+            pcGnb();
+        }else{ 
+            mGnb(); 
+        }
+    });
+    
+    $(window).trigger('resize');
+    
+});
+
+
